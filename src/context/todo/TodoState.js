79398a775d6cd7a23425ecdc2f7129bcb1b53fdc
Eugene <evgeniq.makcimov@yandex.ru>
2021-07-24 19:11:30 +0300

    adding a loading indicator

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -60,6 +60,7 @@ export const TodoState = ({ children }) => {
   }
 
   const fetchTodos = async () => {
+     showLoader()
     const response = await fetch(
       'https://rn-todo-52c55-default-rtdb.europe-west1.firebasedatabase.app/todos.json',
       {
@@ -68,9 +69,10 @@ export const TodoState = ({ children }) => {
       }
     )
     const data = await response.json()
-    console.log('Fetch data', data)
+    // console.log('Fetch data', data)
     const todos = Object.keys(data).map((key) => ({ ...data[key], id: key }))
-    setTimeout(() => dispatch({ type: FETCH_TODOS, todos }), 5000)
+    dispatch({ type: FETCH_TODOS, todos })
+      hideLoader()
   }
 
   const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title })
